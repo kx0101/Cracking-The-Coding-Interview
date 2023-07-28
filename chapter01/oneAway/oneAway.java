c28e10981096eb6cf75c471d555ac55a076a1986
kx0101 <liakos.koulaxis@yahoo.com>
2023-07-28 21:16:36 +0300

    oneAway

---
+++ chapter01/oneAway/oneAway.java
@@ -0,0 +1,12 @@
+public class oneAway {
+    public static boolean isOneWay(String s1, String s2) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOneWay("pale", "ple"));
+        System.out.println(isOneWay("pales", "pale"));
+        System.out.println(isOneWay("pale", "bale"));
+        System.out.println(isOneWay("pale", "bake"));
+    }
+}
